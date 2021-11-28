1d1872fe86c280468f906e8b0eee316b29f27dd3
Meritxell Bendicho <meritxey@gmail.com>
2021-11-28 23:31:36 +0100

    change Ayuda

--- Client/client/src/pages/About/About.jsx
+++ Client/client/src/pages/About/About.jsx
@@ -5,11 +5,14 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 const About = () => {
   return (
     <div className="container">
-      <h1 className="title">NOMBRE DEL PROYECTO</h1>
-      <h2 className="subtitle">Este proyecto nace con idea de ayudar a la gente sin hogar de Madrid.</h2>
-      <h2 className="subtitle">Aquí puedes encontrar mucha información de donde acudir para lo que necesites.</h2>
-      <h2 className="subtitle">Somos un grupo de estudiantes de programación los que hemos dado forma a NOMBRE DEL PROYECTO como proyecto final del bootcamp Full Stack.</h2>
+      <h1 className="title">#NoMásCalle</h1>
+      <h2 className="subtitle">El proyecto #NoMásCalle nace con la idea de ayudar a las personas más necesitadas, personas sin hogar, con una vivienda insegura, inadecuada o en riesgo de exclusión social. Somos un grupo de estudiantes de programación que hemos querido poner al servicio de los más desfavorecidos lo aprendido durante el curso. De esta manera hemos creado el sitio web #NoMásCalle, donde acercaremos a esta parte de la sociedad, a veces olvidada, todos los servicios y recursos que ofrece la ciudad de Madrid. Además, te invitamos a colaborar activamente compartiendo todo aquello que pueda sumar.
+Esperamos que todo aquel que pase por aquí, encuentre aquello que busca y pueda mejorar su calidad de vida o la de los que le rodean. 
+</h2>
       <div className="gitcontainer"><GitHubIcon/> <GitHubIcon/> <GitHubIcon/> <GitHubIcon/></div>
+    <div>
+      <h2></h2>
+    </div>
     </div>
   );
 };
--- Client/client/src/pages/Ayuda/Ayuda.jsx
+++ Client/client/src/pages/Ayuda/Ayuda.jsx
@@ -3,32 +3,80 @@ import "./Ayuda.scss";
 
 const Ayuda = () => {
   return (
-    <div className="enlaces">
-        <h1>CACACACACACA</h1>
-        <h2>RECURSOS PARA PERSONAS SIN HOGAR</h2>
-      <ul>
-        <li>
-          <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Personas-sin-hogar/Programa-de-atencion/Programa-de-atencion/?vgnextfmt=default&vgnextoid=c83f70c0b6a6b310VgnVCM1000000b205a0aRCRD&vgnextchannel=bd9d9b6bf78b8310VgnVCM2000000c205a0aRCRD">
-            Programa de atención
+    <div className="helpWrapper">
+      <h1>Lugares oficiales de recursos disponibles para personas sin hogar</h1>
+      <section>
+        <div className="imageWrapper">
+          <img src='./homeless.jpg' alt=''/>
+        </div>
+        <div className="textWrapper">
+          <h2>Programa de atención</h2>
+          <p>
+            Actuación institucional que busca dar respuesta a las personas que sufren los estados más graves de la exclusión social.
+          </p>
+          <button>
+            <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Personas-sin-hogar/Programa-de-atencion/Programa-de-atencion/?vgnextfmt=default&vgnextoid=c83f70c0b6a6b310VgnVCM1000000b205a0aRCRD&vgnextchannel=bd9d9b6bf78b8310VgnVCM2000000c205a0aRCRD">Enlace</a>
+          </button>
+        </div>
+      </section>
+      <section>
+        <div className="imageWrapper">
+          <img src='./homeless1.jpg' alt=''/>
+        </div>
+        <div className="textWrapper">
+          <h2>Samur Social</h2>
+          <p>
+            Es el Servicio Social de Atención Municipal a las emergencias sociales, integrado en la red de respuestas de los servicios de emergencias de la ciudad de Madrid.
+          </p>
+          <button>
+            <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Samur-Social/?vgnextfmt=default&vgnextchannel=ddf8f471d18b8310VgnVCM2000000c205a0aRCRD">Enlace</a>
+          </button>
+        </div>
+      </section>
+      <section>
+        <div className="imageWrapper">
+          <img src='homeless3.jpg' alt=''/>
+        </div>
+        <div className="textWrapper">
+          <h2>Campaña contra el frio 2021-2022</h2>
+          <p>
+            Busca dar respuesta a las personas sin hogar y que como consecuencia del frio, se produce un incremento  considerable de su vulnerabilidad personal y física.
+          </p>
+          <button>
+            <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Personas-sin-hogar/Campana-contra-el-frio/Campana-municipal-contra-el-frio-2021-2022/?vgnextfmt=default&vgnextoid=c2bedbb5cf70c710VgnVCM2000001f4a900aRCRD&vgnextchannel=e8619b6bf78b8310VgnVCM2000000c205a0aRCRD">Enlace</a>
+          </button>
+        </div>
+      </section>
+      <section>
+        <div className="imageWrapper">
+          <img src='./homeless2.jpg' alt=''/>
+        </div>
+        <div className="textWrapper">
+          <h2>Recursos y actuaciones</h2>
+          <p>
+            Información completa
+          </p>
+          <button>
+            <a href="https://www.comunidad.madrid/servicios/asuntos-sociales/actuaciones-dirigidas-personas-hogar">Enlace</a>
+          </button>
+        </div>
+      </section>
+      <section>
+        <div className="imageWrapper">
+          <img src='homeless4.jpg' alt=''/>
+        </div>
+        <div className="textWrapper">
+          <h2>Plan de inclusión de personas sin hogar</h2>
+          <p>
+            Actuación institucional que busca dar respuesta a las personas que sufren los estados más graves de la exclusión social.
+          </p>
+          <button>
+          <a href="./pdf.pdf" download>
+            Descargar PDF
           </a>
-        </li>
-        <li>
-          <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Samur-Social/?vgnextfmt=default&vgnextchannel=ddf8f471d18b8310VgnVCM2000000c205a0aRCRD">
-            Samur social
-          </a>
-        </li>
-        <li>
-          <a href="https://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Samur-Social-Personas-sin-hogar/Personas-sin-hogar/Campana-contra-el-frio/Campana-municipal-contra-el-frio-2021-2022/?vgnextfmt=default&vgnextoid=c2bedbb5cf70c710VgnVCM2000001f4a900aRCRD&vgnextchannel=e8619b6bf78b8310VgnVCM2000000c205a0aRCRD">
-            Campaña contra el frio 2021-2022
-          </a>
-        </li>
-      </ul>
-      <h2>Aquí tiene tu guía para ser un perfecto sin techo</h2>
-      <a href="./pdf.pdf" download>
-        Descargar PDF
-      </a>
-      <h2>Y de regalo el starter pack del verdadero homeless</h2>
-      <img src="./homeless.jpg" alt="" />
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
