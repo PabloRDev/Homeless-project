134ecf57e2cced28d25b130841da75a051ac7e41
Jesus Rojas <jarojas0107@gmail.com>
2021-11-29 17:12:51 +0100

    Home

--- Client/client/src/pages/Home/Home.jsx
+++ Client/client/src/pages/Home/Home.jsx
@@ -4,28 +4,34 @@ import "../Home/Home.scss";
 const Home = () => {
   return (
     <div>
-      <div>
-        <h1 className="bienvenido">¡Bienvenido a infoHogar!</h1>
+      <div className="tittle">
+        <h1 className="bienvenido">#NoMásCalle</h1>
         <div>
           <img
-            src="https://www.casanc.org/wp-content/uploads/home3.png"
+            src="https://media.discordapp.net/attachments/906549565740679268/914895933718806609/1.png?width=1520&height=772"
             alt="homelogo"
           />
         </div>
         <h2 className="subtitulo">
-          Encuentra un lugar para refugiarte bla bla bla
+        El proyecto #NoMásCalle nace con la idea de ayudar a las personas sin
+          hogar, con una vivienda insegura, inadecuada o en riesgo de exclusión
+          social a no acabar en la calle y sufrir toda la problemática que ello
+          conlleva, acercándoles todas las medidas y eventos que Madrid, sus
+          insituciones y ciudadanos ponen a su disposición. Se calcula que
+          diariamente duermen en las calles de Madrid un millar de personas,
+          este proyecto es por ustedes.
         </h2>
       </div>
 
       <div>
         <iframe
-          width="900"
-          height="505"
-          src="https://www.youtube.com/embed/WOT5R1Inul0"
+          width="859"
+          height="483"
+          src="https://www.youtube.com/embed/kbEhdfv_Qjo"
           title="YouTube video player"
-          frameBorder="0"
+          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
+          allowfullscreen
         ></iframe>
       </div>
     </div>
